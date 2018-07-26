5a08b06d14e572ce94468e680439a9b132aafb90
ibbo8800 <benoit.boulch@pole-emploi.fr>
2018-07-26 16:04:44 +0200

    Initialisation

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Post } from './models/post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'My First Angular Blog...';
+
+  posts: Post[] = [
+    new Post('Mon premier blog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis', 0, new Date()),
+    new Post('Mon deuxième blog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis', 1, new Date()),
+    new Post('Mon troisième blog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis', -1, new Date())
+  ];
+
+}
